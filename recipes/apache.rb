f0a7ed33f76d98381ee17429e4f54130088dbef5
John Stange <john.stange@eglobaltech.com>
2020-12-17 22:07:02 +0000

    rip out the rest of the dependency on the apache2 cookbook, I think

--- recipes/apache.rb
+++ recipes/apache.rb
@@ -18,60 +18,9 @@
 
 node.default['nagios']['server']['web_server'] = 'apache'
 
-#include_recipe 'apache2'
-#include_recipe 'apache2::mod_cgi'
-#include_recipe 'apache2::mod_rewrite'
-#include_recipe 'apache2::mod_php'
-#include_recipe 'apache2::mod_ssl' if node['nagios']['enable_ssl']
-#
-#apache_site '000-default' do
-#  enable false
-#end
-#
-#template "#{node['apache']['dir']}/sites-available/#{node['nagios']['server']['vname']}.conf" do
-#  source 'apache2.conf.erb'
-#  mode '0644'
-#  variables(
-#    nagios_url: node['nagios']['url'],
-#    https: node['nagios']['enable_ssl'],
-#    ssl_cert_file: node['nagios']['ssl_cert_file'],
-#    ssl_cert_key: node['nagios']['ssl_cert_key']
-#  )
-#  if File.symlink?("#{node['apache']['dir']}/sites-enabled/#{node['nagios']['server']['vname']}.conf")
-#    notifies :restart, 'service[apache2]'
-#  end
-#end
-#
-#file "#{node['apache']['dir']}/conf.d/#{node['nagios']['server']['vname']}.conf" do
-#  action :delete
-#end
-#
-#apache_site node['nagios']['server']['vname'] do
-#  notifies :restart, 'service[apache2]'
-#end
-
-node.default['nagios']['web_user'] = node['apache']['user']
-node.default['nagios']['web_group'] = node['apache']['group'] || node['apache']['user']
-
-# configure the appropriate authentication method for the web server
-case node['nagios']['server_auth_method']
-when 'openid'
-  include_recipe 'apache2::mod_auth_openid'
-when 'cas'
-  include_recipe 'apache2::mod_auth_cas'
-when 'ldap'
-  if node['platform_family'] == "rhel" and node['platform_version'].split('.')[0].to_i == 6
-    package "httpd24-mod_ldap" # XXX this assumption is specific to cloudamatic/mu, do not merge upstream
-    apache_module 'ldap' do
-      conf true
-    end
-  else
-    include_recipe "apache2::mod_authnz_ldap"
-  end
-when 'htauth'
-  Chef::Log.info('Authentication method htauth configured in server.rb')
-else
-  Chef::Log.info('Default method htauth configured in server.rb')
+if node['apache']
+  node.default['nagios']['web_user'] = node['apache']['user']
+  node.default['nagios']['web_group'] = node['apache']['group'] || node['apache']['user']
 end
 
 include_recipe 'mu-nagios::server'
