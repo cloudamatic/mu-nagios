6dbe0a57fae740e1e216a2708bd105eb6c59e7fb
John Stange <john.stange@eglobaltech.com>
2020-12-17 17:43:16 +0000

    just... stop trying to touch apache

--- recipes/apache.rb
+++ recipes/apache.rb
@@ -18,37 +18,37 @@
 
 node.default['nagios']['server']['web_server'] = 'apache'
 
-include_recipe 'apache2'
-include_recipe 'apache2::mod_cgi'
-include_recipe 'apache2::mod_rewrite'
-include_recipe 'apache2::mod_php'
-include_recipe 'apache2::mod_ssl' if node['nagios']['enable_ssl']
-
-apache_site '000-default' do
-  enable false
-end
-
-template "#{node['apache']['dir']}/sites-available/#{node['nagios']['server']['vname']}.conf" do
-  source 'apache2.conf.erb'
-  mode '0644'
-  variables(
-    nagios_url: node['nagios']['url'],
-    https: node['nagios']['enable_ssl'],
-    ssl_cert_file: node['nagios']['ssl_cert_file'],
-    ssl_cert_key: node['nagios']['ssl_cert_key']
-  )
-  if File.symlink?("#{node['apache']['dir']}/sites-enabled/#{node['nagios']['server']['vname']}.conf")
-    notifies :restart, 'service[apache2]'
-  end
-end
-
-file "#{node['apache']['dir']}/conf.d/#{node['nagios']['server']['vname']}.conf" do
-  action :delete
-end
-
-apache_site node['nagios']['server']['vname'] do
-  notifies :restart, 'service[apache2]'
-end
+#include_recipe 'apache2'
+#include_recipe 'apache2::mod_cgi'
+#include_recipe 'apache2::mod_rewrite'
+#include_recipe 'apache2::mod_php'
+#include_recipe 'apache2::mod_ssl' if node['nagios']['enable_ssl']
+#
+#apache_site '000-default' do
+#  enable false
+#end
+#
+#template "#{node['apache']['dir']}/sites-available/#{node['nagios']['server']['vname']}.conf" do
+#  source 'apache2.conf.erb'
+#  mode '0644'
+#  variables(
+#    nagios_url: node['nagios']['url'],
+#    https: node['nagios']['enable_ssl'],
+#    ssl_cert_file: node['nagios']['ssl_cert_file'],
+#    ssl_cert_key: node['nagios']['ssl_cert_key']
+#  )
+#  if File.symlink?("#{node['apache']['dir']}/sites-enabled/#{node['nagios']['server']['vname']}.conf")
+#    notifies :restart, 'service[apache2]'
+#  end
+#end
+#
+#file "#{node['apache']['dir']}/conf.d/#{node['nagios']['server']['vname']}.conf" do
+#  action :delete
+#end
+#
+#apache_site node['nagios']['server']['vname'] do
+#  notifies :restart, 'service[apache2]'
+#end
 
 node.default['nagios']['web_user'] = node['apache']['user']
 node.default['nagios']['web_group'] = node['apache']['group'] || node['apache']['user']
